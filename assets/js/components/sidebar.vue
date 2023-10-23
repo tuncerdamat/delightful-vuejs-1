1c36adbcc81abc11d664a6cb0a9bf3d5881cd411
tuncerdamat <tuncer.damat@gmail.com>
2023-10-23 13:04:28 +0200

    Collapse data with onlick event

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -1,5 +1,8 @@
 <template>
-    <div :class="[$style.sidebar, 'p-3', 'mb-5']">
+    <div
+        :class="[$style.sidebar, 'p-3', 'mb-5']"
+        :style="{width: collapsed ? '70px' : 'auto'}"
+    >
         <h5 class="text-center">
             Categories
         </h5>
@@ -23,6 +26,16 @@
                 </a>
             </li>
         </ul>
+
+        <hr>
+
+        <div class="d-flex justify-content-end">
+            <button
+                class="btn btn-secondary btn-sm"
+                @click="toggleCollapsed"
+                v-text="collapsed ? '>>' : '<< Collapse'"
+            />
+        </div>
     </div>
 </template>
 
@@ -31,6 +44,7 @@ export default {
     name: 'Sidebar',
     data() {
         return {
+            collapsed: false,
             categories: [
                 {
                     name: 'Dot Matrix Printers',
@@ -42,6 +56,11 @@ export default {
                 },
             ],
         };
+    },
+    methods: {
+        toggleCollapsed() {
+            this.collapsed = !this.collapsed;
+        },
     },
 };
 </script>
