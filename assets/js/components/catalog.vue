59f67a4a42cdbf6f1b8b0e98f0d9797e5ed3b64d
tuncerdamat <tuncer.damat@gmail.com>
2023-11-01 21:00:18 +0100

    Fetch the products using Axios HTTP Client

--- assets/js/components/catalog.vue
+++ assets/js/components/catalog.vue
@@ -10,8 +10,12 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-xs-12 col-6 mb-2 pb-2">
-                TODO - load some products!
+            <div
+                v-for="product in products"
+                :key="product['@id']"
+                class="col-xs-12 col-6 mb-2 pb-2"
+            >
+                {{ product.name }}
             </div>
         </div>
         <div class="row">
@@ -21,6 +25,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import LegendComponent from '@/components/legend';
 
 export default {
@@ -30,8 +35,14 @@ export default {
     },
     data() {
         return {
+            products: [],
             legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
         };
+    },
+    async mounted() {
+        const response = await axios.get('/api/products');
+
+        this.products = response.data['hydra:member'];
     },
 };
 </script>
