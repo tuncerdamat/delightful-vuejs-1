6069161e3b422afa6d1d4e929f77d86f4ee761d2
tuncerdamat <tuncer.damat@gmail.com>
2023-11-01 21:16:18 +0100

    Product Listing Components

---
+++ assets/js/components/product-list/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <product-card
+            v-for="product in products"
+            :key="product['@id']"
+            :product="product"
+        />
+    </div>
+</template>
+
+<script>
+import ProductCard from '@/components/product-list/product-card';
+
+export default {
+    name: 'ProductList',
+    components: {
+        ProductCard,
+    },
+    props: {
+        products: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
